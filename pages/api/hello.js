2bc83b4c910d209104b2e2f9750391e2b9a774e3
Jaime Dias <jaimejosediasnt@gmail.com>
2022-09-12 01:42:58 -0300

    implantação inicial da libgen

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const libgen = require('libgen');
 
-export default function handler(req, res) {
+class ApiClient {
+  async search(options) {
+    try {
+      const data = await libgen.search(options)
+      return data
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
+};
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     // Process a POST request
   } else {
-    res.status(200).json({ name: 'John Doe' })
+    //res.status(200).json({ name: 'John Doe' })
+    const options = {
+      mirror: 'http://gen.lib.rus.ec',
+      query: 'cats',
+      count: 5,
+      sort_by: 'year',
+      reverse: true
+    }
+    try {
+      const dados = await new ApiClient().search(options)
+      res.json(dados);
+    } catch (e) {
+      console.log(e);
+    };
   }
 }
