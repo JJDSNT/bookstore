f4af3090afba60151bc8d864400d4458903d8a26
Jaime Dias <jaimejosediasnt@gmail.com>
2022-10-16 09:58:49 -0300

    teste rapido

--- pages/read/[cid].js
+++ pages/read/[cid].js
@@ -6,27 +6,27 @@ import { ReactReader } from "react-reader";
 
 //href={`https://ipfs.io/ipfs/${book.ipfs_cid}?filename=${book.title}.${book.extension}`}
 
-const BookReaderPage = ({name,cid}) => {
+const BookReaderPage = ({ name, cid }) => {
 
     const router = useRouter();
     const { asPath } = useRouter();
 
-    const webpagetile = name +' BookStore';
+    const webpagetile = 'BookStore - '+name;
     //const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
     const origin = 'https://bookstore-gamma.vercel.app/';
     const fullPath = `${origin}${asPath}`;
 
     let url = `https://ipfs.io/ipfs/${cid}?filename=book.epub`;
-    
-    useEffect(() => {
-        if (!cid) {
-            return;
-        }
-        url = `https://ipfs.io/ipfs/${cid}?filename=book.epub`;
-    }, [cid])
+
+    // useEffect(() => {
+    //     if (!cid) {
+    //         return;
+    //     }
+    //     url = `https://ipfs.io/ipfs/${cid}?filename=book.epub`;
+    // }, [cid])
     //useeffect para atualizar o cid?
 
-    let teste = `https://ipfs.io/ipfs/bafykbzaceaatvijwreyc6yyarhkq7cpc4nnhxgvamy2xc7argn33ewztqksnq?filename=%28Oxford%20History%20of%20Modern%20Europe%29%20Paul%20W.%20Schroeder%20-%20The%20Transformation%20of%20European%20Politics%201763-1848-Oxford%20University%20Press%2C%20USA%20%281994%29.epub`;
+    //let teste = `https://ipfs.io/ipfs/bafykbzaceaatvijwreyc6yyarhkq7cpc4nnhxgvamy2xc7argn33ewztqksnq?filename=%28Oxford%20History%20of%20Modern%20Europe%29%20Paul%20W.%20Schroeder%20-%20The%20Transformation%20of%20European%20Politics%201763-1848-Oxford%20University%20Press%2C%20USA%20%281994%29.epub`;
 
     // And your own state logic to persist state
     const [location, setLocation] = useState(null)
@@ -38,26 +38,25 @@ const BookReaderPage = ({name,cid}) => {
     return (
         <>
             <Head>
-                <title>{ webpagetile }</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+                <meta name="referrer" content="no-referrer" />
+                
+                <title>{webpagetile}</title>
                 <meta name="description" content={name} />
                 <link rel="icon" href="/favicon.ico" />
 
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
-                <meta name="referrer" content="no-referrer" />
-
-                <meta property="og:url" content={ fullPath } />
+                <meta property="og:url" content={fullPath} />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Bookstore" />
                 <meta property="og:description" content={name} />
-                <meta property="og:image" content="https://libgen.rs/covers/3391000/015805d053b16b1248c1b28d906b84bb-g.jpg" rel="noreferrer" key="ogimage" />
+                <meta property="og:image" content="https://libgen.rs/covers/3391000/015805d053b16b1248c1b28d906b84bb-g.jpg" key="ogimage" />
 
                 <meta name="twitter:card" content="summary" />
-                <meta name="twitter:site" content="@flickr" />
                 <meta property="twitter:domain" content="bookstore-gamma.vercel.app/" />
-                <meta property="twitter:url" content="https://bookstore-gamma.vercel.app/{asPath}" />
+                <meta property="twitter:url" content={fullPath} />
                 <meta name="twitter:title" content="Bookstore" />
                 <meta name="twitter:description" content={name} key="desc" />
-                <meta name="twitter:image" content="https://libgen.rs/covers/3391000/015805d053b16b1248c1b28d906b84bb-g.jpg" rel="noreferrer" />
+                <meta name="twitter:image" content="https://libgen.rs/covers/3391000/015805d053b16b1248c1b28d906b84bb-g.jpg" />
             </Head>
             <div style={{ height: "100vh" }}>
                 <ReactReader
@@ -74,7 +73,7 @@ const BookReaderPage = ({name,cid}) => {
     )
 }
 
-BookReaderPage.getInitialProps = async (ctx) => {    
+BookReaderPage.getInitialProps = async (ctx) => {
     return { name: ctx.query.name, cid: ctx.query.cid }
 }
 
